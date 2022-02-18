770227914ea70387fe5b718d67dfe82be6dd32c0
Valerio <vtognozzi@gmail.com>
2022-02-18 14:34:29 +0100

    cerco i dati address ma non li trovo

--- datileg.py
+++ datileg.py
@@ -1,5 +1,10 @@
 import pickle
-infile = open('1dati.bin,'rb')
+infile = open('1dati.bin','rb')
 dati = pickle.load(infile)
 infile.close()
 print(dati)
+
+for el in dati:
+    if int.from_bytes(el, 'big'):
+        print("trovato un address valido")
+        
